a4f573851189cbfc0b3dff5f4775ac3d690c3576
Nelunika Nuwanthi <nelunikanuwanthi@gmail.com>
2026-02-01 23:09:23 +0530

    QA automation framework structure

--- src/test/java/com/qaautomation/runners/TestRunner.java
+++ src/test/java/com/qaautomation/runners/TestRunner.java
@@ -1,4 +1,4 @@
-package com.qaautomation.runner;
+package com.qaautomation.runners;
 
 import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = "com.qaautomation.stepDefinition",
+        glue = "com.qaautomation.stepdefinitions",
         plugin = {"pretty", "html:target/cucumber-report.html"},
         monochrome = true
 )
--- src/test/java/com/qaautomation/stepdefinition/SalesUserSteps.java
+++
@@ -1,5 +0,0 @@
-package com.qaautomation.stepdefinition;
-
-public class SalesUserSteps {
-    
-}
