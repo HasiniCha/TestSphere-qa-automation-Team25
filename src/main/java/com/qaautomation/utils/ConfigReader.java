6582941329c8e6d087a0f117ef06f9ff25e8ad63
Nelunika Nuwanthi <nelunikanuwanthi@gmail.com>
2026-02-01 23:39:24 +0530

    Initial commit

---
+++ src/main/java/com/qaautomation/utils/ConfigReader.java
@@ -0,0 +1,37 @@
+package com.qaautomation.utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigReader {
+
+    private static Properties properties = new Properties();
+
+    static {
+        try {
+            FileInputStream fis = new FileInputStream("src/test/resources/config/config.properties");
+            properties.load(fis);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to load config.properties file.");
+        }
+    }
+
+    public static String get(String key) {
+        String value = properties.getProperty(key);
+        if (value != null) {
+            return value.trim();
+        } else {
+            throw new RuntimeException("Property '" + key + "' not found in config.properties");
+        }
+    }
+
+    public static int getInt(String key) {
+        return Integer.parseInt(get(key));
+    }
+
+    public static boolean getBoolean(String key) {
+        return Boolean.parseBoolean(get(key));
+    }
+}
---
+++ src/test/resources/features/SalesAdmin.feature
@@ -0,0 +1,3 @@
+Feature: Sales Admin UI
+
+  
